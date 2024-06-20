04dda8e9305e54e573e7d826a1673d0f9e2885ca
hang <2283582431@qq.com>
2024-06-20 16:03:14 +0800

    feat: toRefs, proxyRef实现

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -12,7 +12,7 @@ export function createRef(value: any) {
 
 class RefImpl {
     // 增加ref标识
-    _v_isRef = true;
+    __v_isRef = true;
     // 存放值
     _value;
     // 存放依赖
@@ -47,5 +47,51 @@ function triggerRefValue(ref: RefImpl) {
     let dep = ref.dep;
     if (dep) {
         triggerEffects(dep);
+    }
+}
+
+// toRef toRefs
+export function toRef(object: any, key: any) {
+    return new ObjectRefImpl(object, key);
+}
+
+export function toRefs(object: any) {
+    const res = {} as any;
+    for (const key in object) {
+        res[key] = toRef(object, key);
+    }
+    return res;
+}
+
+export function proxyRefs(objectWithRef: any) {
+    return new Proxy(objectWithRef, {
+        get(target, key, receiver) {
+            let res = Reflect.get(target, key, receiver);
+            return res.__v_isRef ? res.value : res; // 如果是ref 就返回.value 自动脱ref
+        },
+        set(target, key, value, receiver) {
+            const oldVal = target[key];
+
+            if (oldVal.__v_isRef) {
+                oldVal.value = value; // 如果老值是ref 就直接赋值
+                return true;
+            } else {
+                return Reflect.set(target, key, value, receiver);
+            }
+        },
+    });
+}
+
+class ObjectRefImpl {
+    // 增加ref标识
+    __v_isRef = true;
+    constructor(public _object: any, public _key: any) {}
+
+    get value() {
+        return this._object[this._key];
+    }
+
+    set value(newValue: any) {
+        this._object[this._key] = newValue;
     }
 }
